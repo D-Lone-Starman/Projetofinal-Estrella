8954c4ef2995b2e7ff3060c6fbbb616f660ac0ab
D.Lone_Starman <ogkowar1@gmail.com>
2025-08-15 19:47:00 -0300

    firebase implementada

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -17,5 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfifireg);
 const analytics = getAnalytics(app);
